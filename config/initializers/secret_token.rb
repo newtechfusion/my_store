884bf99cf864e88d91a7416e03bfb0917b17a343
NTF <gary@newtechfusion.com>
2013-01-12 11:25:26 +0530

    initial commit with spree

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mystore::Application.config.secret_token = '0acbaf6670686407a4ed850904c5917c722c149724e342267ae833d6b0b035db05473e8762fbe3e15a27b01c06ba455ddcde9000925a155af85b57fa6e63b6b9'
